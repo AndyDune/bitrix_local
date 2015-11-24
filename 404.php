8dbae12d2d4dba4953afc763bc071b9953c74dd7
Andrey Ryzhov <dune@rznlf.ru>
2015-11-24 12:53:20 +0300

    Изменил подход к собсьвенным инстрециях для urlrewrite - свой подключается в файле 404.php

---
+++ 404.php
@@ -0,0 +1,16 @@
+<?
+use Rzn\Library\Registry;
+
+// Обработка собственных редиректов
+include($_SERVER['DOCUMENT_ROOT'] . '/local/urlrewrite.php');
+
+include_once($_SERVER['DOCUMENT_ROOT'].'/bitrix/modules/main/include/urlrewrite.php');
+
+CHTTP::SetStatus("404 Not Found");
+@define("ERROR_404","Y");
+
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+
+include(Registry::get('TEMPLATE_ROOT') . '/404.php');
+
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
